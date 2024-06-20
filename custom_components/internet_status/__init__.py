62d006baf8126d9eab670cf12ffe363db9495d35
Crowbar Z <crowbarz@outlook.com>
2024-06-20 22:49:44 +0100

    Move update listener function into async_setup_entry

--- custom_components/internet_status/__init__.py
+++ custom_components/internet_status/__init__.py
@@ -1,4 +1,5 @@
 """Monitor internet link status via DNS queries."""
+
 from __future__ import annotations
 
 from homeassistant.config_entries import ConfigEntry
@@ -21,14 +22,13 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
-    entry.async_on_unload(entry.add_update_listener(_async_update_listener))
+    async def async_update_options(hass: HomeAssistant, entry: ConfigEntry) -> None:
+        """Handle options update."""
+        await hass.config_entries.async_reload(entry.entry_id)
+
+    entry.async_on_unload(entry.add_update_listener(async_update_options))
 
     return True
-
-
-async def _async_update_listener(hass: HomeAssistant, entry: ConfigEntry) -> None:
-    """Handle options update."""
-    await hass.config_entries.async_reload(entry.entry_id)
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
