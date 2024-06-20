06c163b4287bc1d2f24b79024b977ee7e95bc46c
Crowbar Z <crowbarz@outlook.com>
2024-06-20 20:41:35 +0100

    Wait for InternetStatusCoordinator to refresh before setting up platform

--- custom_components/internet_status/__init__.py
+++ custom_components/internet_status/__init__.py
@@ -15,8 +15,8 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up internet_status from a config entry."""
 
     hass.data.setdefault(DOMAIN, {})
-
     coordinator = InternetStatusCoordinator(hass, entry, entry.options)
+    await coordinator.async_config_entry_first_refresh()
     hass.data[DOMAIN][entry.entry_id] = coordinator
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
